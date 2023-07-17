0531ead3e6dc9dd5a90d394326b51c7f205646e7
Cyber Knight <cyberknight755@gmail.com>
2023-07-17 22:43:19 +0800

    main: Refactor for consistency
    - Cleanup some unnecessary variables.
    - Enforce usage of LLVM and LLVM_IAS for clang.
    - Utilize Neutron Clang.
    - Dynamically handle kver and zipn.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- main.sh
+++ main.sh
@@ -43,8 +43,7 @@ COMMIT_HASH=$(git rev-parse --short HEAD)
 export COMMIT_HASH
 
 # Build status. Set 1 for release builds. | Set 0 for bleeding edge builds.
-export RELEASE=$rel
-if [ "${RELEASE}" = 1 ]; then
+if [ "${RELEASE}" == 1 ]; then
     export STATUS="Release"
     export CHATID=-1001361882613
     export re="rc"
@@ -57,26 +56,22 @@ fi
 # Telegram Information. Set 1 to enable. | Set 0 to disable.
 export TGI=1
 
-# Necessary variables to be exported.
-export ci
-export version
-
 # Number of jobs to run.
 PROCS=$(nproc --all)
 export PROCS
 
 # Compiler to use for builds.
-export COMPILER=gcc
+export COMPILER=clang
 
 # Requirements
-if [ "${ci}" != 1 ]; then
+if [ "${CI}" == 0 ]; then
     if ! hash dialog make curl wget unzip find 2>/dev/null; then
         echo -e "\n\e[1;31m[✗] Install dialog, make, curl, wget, unzip, and find! \e[0m"
         exit 1
     fi
 fi
 
-if [[ ${COMPILER} == gcc ]]; then
+if [[ "${COMPILER}" == gcc ]]; then
     if [ ! -d "${KDIR}/gcc64" ]; then
         curl -sL https://github.com/cyberknight777/gcc-arm64/archive/refs/heads/master.tar.gz | tar -xzf -
         mv "${KDIR}"/gcc-arm64-master "${KDIR}"/gcc64
@@ -91,45 +86,33 @@ if [[ ${COMPILER} == gcc ]]; then
     export KBUILD_COMPILER_STRING
     export PATH="${KDIR}"/gcc32/bin:"${KDIR}"/gcc64/bin:/usr/bin/:${PATH}
     MAKE+=(
-        ARCH=arm64
         O=out
         CROSS_COMPILE=aarch64-elf-
         CROSS_COMPILE_ARM32=arm-eabi-
         LD="${KDIR}"/gcc64/bin/aarch64-elf-"${LINKER}"
-        AR=llvm-ar
-        NM=llvm-nm
-        OBJDUMP=llvm-objdump
-        OBJCOPY=llvm-objcopy
-        OBJSIZE=llvm-objsize
-        STRIP=llvm-strip
-        HOSTAR=llvm-ar
-        HOSTCC=gcc
-        HOSTCXX=aarch64-elf-g++
+        AR=aarch64-elf-ar
+        AS=aarch64-elf-as
+        NM=aarch64-elf-nm
+        OBJDUMP=aarch64-elf-objdump
+        OBJCOPY=aarch64-elf-objcopy
         CC=aarch64-elf-gcc
     )
 
-elif [[ ${COMPILER} == clang ]]; then
-    if [ ! -d "${KDIR}/proton-clang" ]; then
-        wget https://github.com/kdrag0n/proton-clang/archive/refs/heads/master.zip
-        unzip "${KDIR}"/master.zip
-        mv "${KDIR}"/proton-clang-master "${KDIR}"/proton-clang
+elif [[ "${COMPILER}" == clang ]]; then
+    if [ ! -d "${KDIR}/neutron-clang" ]; then
+        mkdir "${KDIR}"/neutron-clang
+        cd "${KDIR}"/neutron-clang || exit 1
+        bash <(curl -s "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman") -S
+        cd "${KDIR}" || exit 1
     fi
 
-    KBUILD_COMPILER_STRING=$("${KDIR}"/proton-clang/bin/clang -v 2>&1 | head -n 1 | sed 's/(https..*//' | sed 's/ version//')
+    KBUILD_COMPILER_STRING=$("${KDIR}"/neutron-clang/bin/clang -v 2>&1 | head -n 1 | sed 's/(https..*//' | sed 's/ version//')
     export KBUILD_COMPILER_STRING
-    export PATH=$KDIR/proton-clang/bin/:/usr/bin/:${PATH}
+    export PATH=$KDIR/neutron-clang/bin/:/usr/bin/:${PATH}
     MAKE+=(
-        ARCH=arm64
         O=out
-        CROSS_COMPILE=aarch64-linux-gnu-
-        CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-        LD="${LINKER}"
-        AR=llvm-ar
-        AS=llvm-as
-        NM=llvm-nm
-        OBJDUMP=llvm-objdump
-        STRIP=llvm-strip
-        CC=clang
+	LLVM=1
+	LLVM_IAS=1
     )
 fi
 
@@ -137,27 +120,18 @@ if [ ! -d "${KDIR}/anykernel3-dragonheart/" ]; then
     git clone --depth=1 https://github.com/cyberknight777/anykernel3 -b op7 anykernel3-dragonheart
 fi
 
-if [ "${ci}" != 1 ]; then
-    if [ -z "${kver}" ]; then
-        echo -ne "\e[1mEnter kver: \e[0m"
-        read -r kver
-    else
-        export KBUILD_BUILD_VERSION=${kver}
-    fi
-
-    if [ -z "${zipn}" ]; then
-        echo -ne "\e[1mEnter zipname: \e[0m"
-        read -r zipn
-    fi
-
-else
-    export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
-    export KBUILD_BUILD_HOST=$DRONE_SYSTEM_HOST
-    export KBUILD_BUILD_USER=$BUILDER
-    export VERSION=$version
-    kver=$KBUILD_BUILD_VERSION
-    zipn=DragonHeart-op7-${VERSION}
+if [ ! -f "${KDIR}/version" ]; then
+    echo -e "\n\e[1;31m[✗] version file not found!!! Read https://github.com/cyberknight777/YAKB#version-file for more information.\e[0m"
+    exit 1
 fi
+
+KBUILD_BUILD_VERSION=$(grep num= version | cut -d= -f2)
+export KBUILD_BUILD_VERSION
+export KBUILD_BUILD_USER="cyberknight777"
+export KBUILD_BUILD_HOST="builder"
+VERSION=$(grep ver= version | cut -d= -f2)
+kver="${KBUILD_BUILD_VERSION}"
+zipn=DragonHeart-op7-"${VERSION}"
 
 # A function to exit on SIGINT.
 exit_on_signal_SIGINT() {
@@ -210,7 +184,7 @@ mcfg() {
 
 # A function to build the kernel.
 img() {
-    if [[ ${TGI} != "0" ]]; then
+    if [[ "${TGI}" == "1" ]]; then
         tg "
 *Build Number*: \`${kver}\`
 *Status*: \`${STATUS}\`
@@ -221,7 +195,7 @@ img() {
 *Date*: \`$(date)\`
 *Zip Name*: \`${zipn}\`
 *Compiler*: \`${KBUILD_COMPILER_STRING}\`
-*Linker*: \`$("${KDIR}"/gcc64/bin/aarch64-elf-${LINKER} -v | head -n1 | sed 's/(compatible with [^)]*)//' |
+*Linker*: \`$("${KDIR}"/neutron-clang/bin/${LINKER} -v | head -n1 | sed 's/(compatible with [^)]*)//' |
             head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')\`
 *Branch*: \`$(git rev-parse --abbrev-ref HEAD)\`
 *Last Commit*: [${COMMIT_HASH}](${REPO_URL}/commit/${COMMIT_HASH})
@@ -234,12 +208,12 @@ img() {
     BUILD_END=$(date +"%s")
     DIFF=$((BUILD_END - BUILD_START))
     if [ -f "${KDIR}/out/arch/arm64/boot/Image" ]; then
-        if [[ ${SILENT} != "1" ]]; then
+        if [[ "${TGI}" == "1" ]]; then
             tg "*Kernel Built after $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)*"
         fi
         echo -e "\n\e[1;32m[✓] Kernel built after $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)! \e[0m"
     else
-        if [[ ${TGI} != "0" ]]; then
+        if [[ "${TGI}" == "1" ]]; then
             tgs "log.txt" "*Build failed*"
         fi
         echo -e "\n\e[1;31m[✗] Build Failed! \e[0m"
@@ -257,7 +231,7 @@ dtb() {
 
 # A function to build out-of-tree modules.
 mod() {
-    if [[ ${TGI} != "0" ]]; then
+    if [[ "${TGI}" == "1" ]]; then
         tg "*Building Modules!*"
     fi
     rgn
@@ -272,38 +246,32 @@ mod() {
 
 # A function to build an AnyKernel3 zip.
 mkzip() {
-    if [[ ${TGI} != "0" ]]; then
+    if [[ "${TGI}" == "1" ]]; then
         tg "*Building zip!*"
     fi
     echo -e "\n\e[1;93m[*] Building zip! \e[0m"
     mkdir -p "${KDIR}"/anykernel3-dragonheart/dtbs
     mv "${KDIR}"/out/arch/arm64/boot/dtbo.img "${KDIR}"/anykernel3-dragonheart
-    mv "${KDIR}"/out/arch/arm64/boot/dtb.img "${KDIR}"/anykernel3-dragonheart/dtbs/
-    sed -i 's/ext4/erofs/g' "${KDIR}"/arch/arm64/boot/dts/qcom/sm8150.dtsi
-    sed -i 's/,barrier=1,discard//g' "${KDIR}"/arch/arm64/boot/dts/qcom/sm8150.dtsi
-    time make -j"$PROCS" "${MAKE[@]}" dtb.img
-    mv "${KDIR}"/out/arch/arm64/boot/dtb.img "${KDIR}"/anykernel3-dragonheart/dtbs/dtb_erofs.img
+    mv "${KDIR}"/out/arch/arm64/boot/dtb.img "${KDIR}"/anykernel3-dragonheart
     mv "${KDIR}"/out/arch/arm64/boot/Image "${KDIR}"/anykernel3-dragonheart
     cd "${KDIR}"/anykernel3-dragonheart || exit 1
     zip -r9 "$zipn".zip . -x ".git*" -x "README.md" -x "LICENSE" -x "*.zip"
     echo -e "\n\e[1;32m[✓] Built zip! \e[0m"
-    if [[ ${CI} != "0" ]]; then
-        git clone https://github.com/cyberknight777/op7_json.git
-        cd op7_json || exit 1
-        echo "https://cyberknight777:$PASSWORD@github.com" >.pwd
-        git config credential.helper "store --file .pwd"
-        sha1=$(sha1sum ../"${zipn}".zip | cut -d ' ' -f1)
-        if [[ ${RELEASE} != "1" ]]; then
-            rm changelog_r.md
-            wget "${link}/raw" -O changelog_r.md
-            gh release create "${version}" -t "DragonHeart for $CODENAME [BLEEDING EDGE] - $version"
-            gh release upload "${version}" ../"${zipn}.zip"
-            echo "
+    if [[ "${OTA}" == "1" ]]; then
+	git clone https://github.com/cyberknight777/op7_json.git
+	cd op7_json || exit 1
+	echo "https://cyberknight777:$PASSWORD@github.com" > .pwd
+	git config credential.helper "store --file .pwd"
+	sha1=$(sha1sum ../"${zipn}".zip | cut -d ' ' -f1)
+	if [[ "${RELEASE}" != "1" ]]; then
+	    rm changelog_r.md
+	    wget "${CL_LINK}/raw" -O changelog_r.md
+	    echo "
 {
   \"kernel\": {
   \"name\": \"DragonHeart\",
-  \"version\": \"$version\",
-  \"link\": \"https://github.com/cyberknight777/op7_json/releases/download/$version/$zipn.zip\",
+  \"version\": \"$VERSION\",
+  \"link\": \"https://github.com/cyberknight777/op7_json/releases/download/$VERSION/$zipn.zip\",
   \"changelog_url\": \"https://raw.githubusercontent.com/cyberknight777/op7_json/master/changelog_r.md\",
   \"date\": \"$DATE\",
   \"sha1\": \"$sha1\"
@@ -312,20 +280,22 @@ mkzip() {
     \"link\": \"https://t.me/knightschat\"
   }
 }
-" >DragonHeart-r.json
-            git add DragonHeart-r.json changelog_r.md || exit 1
-            git commit -s -m "DragonHeart: Update $CODENAME to $version release" -m "- This is a bleeding edge release."
-        else
-            rm changelog.md
-            wget "${link}"/raw -O changelog.md
-            gh release create "${version}" -t "DragonHeart for $CODENAME [RELEASE] - $version"
-            gh release upload "${version}" ../"${zipn}.zip"
-            echo "
+" > DragonHeart-r.json
+	    git add DragonHeart-r.json changelog_r.md || exit 1
+	    git commit -s -m "DragonHeart: Update $CODENAME to $VERSION release" -m "- This is a bleeding edge release."
+            git commit --amend --reset-author --no-edit
+            git push
+            gh release create "${VERSION}" -t "DragonHeart for $CODENAME [BLEEDING EDGE] - $VERSION"
+            gh release upload "${VERSION}" ../"${zipn}.zip"
+	else
+	    rm changelog.md
+	    wget "${CL_LINK}"/raw -O changelog.md
+	    echo "
 {
   \"kernel\": {
   \"name\": \"DragonHeart\",
-  \"version\": \"$version\",
-  \"link\": \"https://github.com/cyberknight777/op7_json/releases/download/$version/$zipn.zip\",
+  \"version\": \"$VERSION\",
+  \"link\": \"https://github.com/cyberknight777/op7_json/releases/download/$VERSION/$zipn.zip\",
   \"changelog_url\": \"https://raw.githubusercontent.com/cyberknight777/op7_json/master/changelog.md\",
   \"date\": \"$DATE\",
   \"sha1\": \"$sha1\"
@@ -334,18 +304,22 @@ mkzip() {
     \"link\": \"https://t.me/knightschat\"
   }
 }
-" >DragonHeart-rc.json
-            git add DragonHeart-rc.json changelog.md || exit 1
-            git commit -s -m "DragonHeart: Update $CODENAME to $version release" -m "- This is a stable release."
-        fi
-        git push
-        cd ../ || exit 1
+" > DragonHeart-rc.json
+	    git add DragonHeart-rc.json changelog.md || exit 1
+	    git commit -s -m "DragonHeart: Update $CODENAME to $VERSION release" -m "- This is a stable release."
+            git commit --amend --reset-author --no-edit
+            git push
+            gh release create "${VERSION}" -t "DragonHeart for $CODENAME [RELEASE] - $VERSION"
+            gh release upload "${VERSION}" ../"${zipn}.zip"
+	fi
+	cd ../ || exit 1
     fi
-    if [[ ${TGI} != "0" ]]; then
+    if [[ "${TGI}" == "1" ]]; then
         tgs "${zipn}.zip" "*#${kver} ${KBUILD_COMPILER_STRING}*"
-        tg "
-*OTA*: https://raw.githubusercontent.com/cyberknight777/op7\_json/master/DragonHeart-${re}.json
+	tg "
+*Build*: https://github.com/cyberknight777/op7\_json/releases/download/$VERSION/$zipn.zip
 *Changelog*: https://github.com/cyberknight777/op7\_json/blob/master/changelog\_${re}.md
+*OTA*: https://raw.githubusercontent.com/cyberknight777/op7\_json/master/DragonHeart-${re}.json
 "
     fi
 }
@@ -363,7 +337,7 @@ upr() {
     echo -e "\n\e[1;93m[*] Bumping localversion to -DragonHeart-${1}! \e[0m"
     "${KDIR}"/scripts/config --file "${KDIR}"/arch/arm64/configs/$CONFIG --set-str CONFIG_LOCALVERSION "-DragonHeart-${1}"
     rgn
-    if [ "${ci}" != 1 ]; then
+    if [ "${CI}" == "0" ]; then
         git add arch/arm64/configs/$CONFIG
         git commit -S -s -m "dragonheart_defconfig: Bump to \`${1}\`"
     fi
@@ -373,14 +347,14 @@ upr() {
 # A function to showcase the options provided for args-based usage.
 helpmenu() {
     echo -e "\n\e[1m
-usage: kver=<version number> zipn=<zip name> $0 <arg>
+usage: bash $0 <arg>
 
-example: $0 --kver=69 --zipn=Kernel-Beta mcfg
-example: $0 --kver=420 --zipn=Kernel-Beta mcfg img
-example: $0 --kver=69420 --zipn=Kernel-Beta mcfg img mkzip
-example: $0 --kver=1 --zipn=Kernel-Beta --obj=drivers/android/binder.o
-example: $0 --kver=2 --zipn=Kernel-Beta --obj=kernel/sched/
-example: $0 --kver=3 --zipn=Kernel-Beta--upr=r16
+example: bash $0 mcfg
+example: bash $0 mcfg img
+example: bash $0 mcfg img mkzip
+example: bash $0 --obj=drivers/android/binder.o
+example: bash $0 --obj=kernel/sched/
+example: bash $0 --upr=r16
 
 	 mcfg   Runs make menuconfig
 	 img    Builds Kernel
@@ -389,9 +363,8 @@ example: $0 --kver=3 --zipn=Kernel-Beta--upr=r16
 	 mkzip  Builds anykernel3 zip
 	 --obj  Builds specific driver/subsystem
 	 rgn    Regenerates defconfig
-	 --upr  Uprevs kernel version in defconfig\e[0m
-	 --kver kernel buildversion
-	 --zipn zip name"
+	 --upr  Uprevs kernel version in defconfig
+\e[0m"
 }
 
 # A function to setup menu-based usage.
@@ -547,8 +520,8 @@ ndialog() {
     esac
 }
 
-if [ "${ci}" == 1 ]; then
-    upr "${version}"
+if [ "${CI}" == 1 ]; then
+    upr "${VERSION}"
 fi
 
 if [[ -z $* ]]; then
@@ -574,7 +547,7 @@ for arg in "$@"; do
         ;;
     "--obj="*)
         object="${arg#*=}"
-        if [[ -z $object ]]; then
+        if [[ -z "$object" ]]; then
             echo "Use --obj=filename.o"
             exit 1
         else
@@ -586,7 +559,7 @@ for arg in "$@"; do
         ;;
     "--upr="*)
         vers="${arg#*=}"
-        if [[ -z $vers ]]; then
+        if [[ -z "$vers" ]]; then
             echo "Use --upr=version"
             exit 1
         else
